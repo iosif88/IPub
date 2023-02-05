ac7344059c231eb835f719adfa17956c09dba443
iosifios <iosifios88@gmail.com>
2023-02-05 13:27:39 +0100

    Initial Commit

---
+++ beerBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beerBar
+//
+//  Created by Iosif Dubikovski on 2/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
